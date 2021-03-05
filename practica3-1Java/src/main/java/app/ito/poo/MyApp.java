792d0fbd811f1f0863e84baf7e91d64a0966cb86
jaque <equipo8@Jaque>
2021-03-05 13:48:19 -0600

    Practica 3-ej1

---
+++ practica3-1Java/src/main/java/app/ito/poo/MyApp.java
@@ -0,0 +1,19 @@
+package app.ito.poo;
+
+import java.util.HashSet;
+import Calzado.ito.poo.ClaseCalzado;
+
+public class MyApp {
+
+	static void run() {
+		
+		System.out.println(new ClaseCalzado());	
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		run();
+	}
+
+}
